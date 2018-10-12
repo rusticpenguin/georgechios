0d4378d5a9ba5073f6d903d92e65825989b70e76
rusticpenguin <georgechios.music@gmail.com>
2018-10-12 16:43:48 -0600

    Added my name and placeholder images

--- georgechios/src/Components/Header.js
+++ georgechios/src/Components/Header.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css';
 
 class Header extends Component {
   render() {
     return ( 
       <header>
+        <div className="MyName">
+          <Link to='/'>
+            <img src="https://via.placeholder.com/90x90" alt="logo" />
+            <h1> George Chios </h1>
+          </Link>
+        </div>
         <nav>
           <ul>
             <li><Link to='/'>Home</Link></li>
             <li><Link to='/about'>About</Link></li>
             <li><Link to='/projects'>Projects</Link></li>
             <li><Link to='/meetups'>Meetups</Link></li>
+            <li><a href="https://www.rusticpenguin.com"><img src="https://via.placeholder.com/90x90" alt="Rustic Penguin"></img></a></li>
           </ul>
         </nav>
       </header>
