e054a1b6f9058e0aa87cb256103436a08c797d4c
rusticpenguin <georgechios.music@gmail.com>
2018-10-13 15:31:58 -0600

    Rebuilt app for deployment

---
+++ georgechios/build/precache-manifest.40644187c3eb441984af927a9cf097a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "945809a7d591e36f5394",
+    "url": "/static/js/main.945809a7.chunk.js"
+  },
+  {
+    "revision": "2b255f5f6dcc7ac52e58",
+    "url": "/static/js/1.2b255f5f.chunk.js"
+  },
+  {
+    "revision": "945809a7d591e36f5394",
+    "url": "/static/css/main.4e0fd58e.chunk.css"
+  },
+  {
+    "revision": "fb0bf3532431b2ed9e6e87d59dc0cbfc",
+    "url": "/index.html"
+  }
+];
