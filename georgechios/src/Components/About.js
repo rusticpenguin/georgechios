c8ba477fe2d76866fe68379006aa8b38232df14f
rusticpenguin <georgechios.music@gmail.com>
2018-10-15 15:38:01 -0600

    Moved home page to about. Added second article. Updated resume button's colors

--- georgechios/src/Components/About.js
+++ georgechios/src/Components/About.js
@@ -4,7 +4,37 @@ class About extends Component {
   render() {
     return ( 
       <div>
-        <h1>About</h1>
+        <section>
+          <h2>Hello, I am George Chios!</h2>
+          <article className="pictureLeft">
+            <img className="articlePicture" src="https://via.placeholder.com/300x300" alt="Profile Picture of George" />
+            <div className="aboutMe">
+              <p>I do a lot of different things. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+              <button className="resumeButton">- My Resume -</button>
+            </div>
+          </article>
+        </section>
+        <hr />
+        <section>
+          <h2>Software Developer, Game Designer, Audio Engineer</h2>
+          <article>
+            <img className="articlePicture" src="https://via.placeholder.com/900x300" alt="Profile Picture of George" />
+            <div className="threeColumns">
+              <section>
+                <h3>Software Development</h3>
+                <p>I do a lot of different things. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              </section>
+              <section>
+                <h3>Game Design</h3>
+                <p>I do a lot of different things. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </section>
+              <section>
+                <h3>Audio Engineering</h3>
+                <p>I do a lot of different things. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              </section>
+            </div>
+          </article>
+        </section>
       </div>
     );
 	}
