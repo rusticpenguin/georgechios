31fa53840420dc64e8372241373decc18463d008
rusticpenguin <georgechios.music@gmail.com>
2018-10-17 14:22:14 -0600

    Styling home page and moved about styling to app.css

--- georgechios/src/Components/Home.js
+++ georgechios/src/Components/Home.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import './Home.css';
+import SocialMedia from './SocialMedia';
 
 class Home extends Component {
   render() {
     return ( 
       <section>
-        <h2>Hello, I am George Chios!</h2>
-        <article>
-          <img className="articlePicture" src="https://via.placeholder.com/300x300" alt="Profile Picture of George" />
-          <div className="aboutMe">
-            <p>I do a lot of different things. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            <button className="resumeButton">- My Resume -</button>
-          </div>
-        </article>
+        <h2 className="welcomeHome">Welcome To My Website!</h2>
+        <hr />
+        <SocialMedia />
+
       </section>
     );
 	}
